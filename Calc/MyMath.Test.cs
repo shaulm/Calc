72e6fa1b8812f9f6a22bcae1442db9ab277f80b9
Administrator <Administrator@EC2AMAZ-LE68PGQ>
2018-11-28 18:58:48 +0000

    gggg

---
+++ Calc/MyMath.Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calc
+{
+    class MyMathTest
+    {
+        [TestFixture]
+
+        class Test_Add
+        {
+            [TestCase]
+
+            public void TestAddPositive()
+            {
+                MyMath math = new MyMath();
+                Assert.AreEqual(30, math.Add(10, 20));
+
+            }
+
+            [TestCase]
+            public void TestAddNegative()
+            {
+                MyMath math = new MyMath();
+                Assert.AreEqual(45, math.Add(10, 20));
+
+            }
+            [TestCase]
+            public void TestAddGreater()
+                {
+                    MyMath math = new MyMath();
+                    Assert.Greater(45, math.Add(10, 20));
+                }
+         
+ 
+        }
+
+
+    }
+}
+
---
+++ Calc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            Console.WriteLine(math.Add(10, 20));
+            Console.ReadLine();
+        }
+    }
+}
